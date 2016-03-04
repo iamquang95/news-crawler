ac678b115ba4a3ec9a27424519afe3faae1c7676
iamquang95 <iamquang95@gmail.com>
2016-03-04 18:33:39 +0700

    Add XMLWriter.java

--- src/main/java/edu/vnu/uet/smm/crawler/VnExpressCrawler.java
+++ src/main/java/edu/vnu/uet/smm/crawler/VnExpressCrawler.java
@@ -9,6 +9,8 @@ import org.jsoup.nodes.Document;
 import edu.vnu.uet.smm.common.base.SMMDocument;
 import edu.vnu.uet.smm.common.util.PropertyLoader;
 import edu.vnu.uet.smm.crawler.extractor.VnExpressExtractor;
+import edu.vnu.uet.smm.crawler.XMLWriter;
+
 import edu.vnu.uet.smm.crawler.fetcher.URLFetcher;
 import edu.vnu.uet.smm.database.elastic.ElasticSearchConnection;
 
@@ -31,36 +33,54 @@ public class VnExpressCrawler {
 	}
 
 	public static void main(String[] args) {
-		String url = "http://vnexpress.net";
-		int loop = 1;
-		boolean isLoop = true;
-		while (isLoop) {
-			System.out.println("Iterator " + loop);
-			try {
-				URLFetcher fetcher = new URLFetcher(url);
-				Set<URL> links = fetcher.getFullLinks(CRAWLER_LEVEL);
-				for (URL link : links) {
-					if (link.getRef() == null && link.getFile().endsWith(".html")) {
-						Document doc = URLFetcher.fetchByJsoup(link.toString(), CRAWLER_TIMEOUT);
-						if (doc == null)
-							continue;
-
-						SMMDocument smmdoc = VnExpressExtractor.extract(doc, IS_ANALYSIS);
-						if (smmdoc != null) {
-							es.createIndexResponse(INDEXNAME, "article", smmdoc.getId(), smmdoc.printJson());
-							if (GET_COMMENT) {
-								List<SMMDocument> smmcomments = VnExpressExtractor.extractComments(smmdoc, IS_ANALYSIS);
-								for (SMMDocument comment : smmcomments)
-									es.createIndexResponse(INDEXNAME, "comment", comment.getId(), comment.printJson());
-							}
-						}
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+		String url = "http://vnexpress.net/tin-tuc/thoi-su/tp-hcm-don-tau-hon-54-000-tan-cap-cang-2991803.html";
+		try{
+			Document doc = URLFetcher.fetchByJsoup(url, CRAWLER_TIMEOUT);
+			if (doc != null) {
+//				System.out.println(VnExpressExtractor.extractContent(doc));
+				SMMDocument smmdoc = VnExpressExtractor.extract(doc, false); // IS_ANALYSIS = False
+				System.out.println(smmdoc.getLink());
+//				System.out.println(smmdoc.getCategory());
+//				//System.out.println(smmdoc.getLastUpdate());
+//				System.out.println(smmdoc.getTitle());
+//				System.out.println(smmdoc.getLike());
+				XMLWriter.writeToFile(smmdoc, "/home/quangle/crawled_data/vnexpress/test.xml");
+				
 			}
-			loop++;
 		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	
+//		String url = "http://vnexpress.net";
+//		int loop = 1;
+//		boolean isLoop = true;
+//		while (isLoop) {
+//			System.out.println("Iterator " + loop);
+//			try {
+//				URLFetcher fetcher = new URLFetcher(url);
+//				Set<URL> links = fetcher.getFullLinks(CRAWLER_LEVEL);
+//				for (URL link : links) {
+//					if (link.getRef() == null && link.getFile().endsWith(".html")) {
+//						Document doc = URLFetcher.fetchByJsoup(link.toString(), CRAWLER_TIMEOUT);
+//						if (doc == null)
+//							continue;
+//
+//						SMMDocument smmdoc = VnExpressExtractor.extract(doc, IS_ANALYSIS);
+//						if (smmdoc != null) {
+//							es.createIndexResponse(INDEXNAME, "article", smmdoc.getId(), smmdoc.printJson());
+//							if (GET_COMMENT) {
+//								List<SMMDocument> smmcomments = VnExpressExtractor.extractComments(smmdoc, IS_ANALYSIS);
+//								for (SMMDocument comment : smmcomments)
+//									es.createIndexResponse(INDEXNAME, "comment", comment.getId(), comment.printJson());
+//							}
+//						}
+//					}
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//			loop++;
+//		}
 	}
-
 }
---
+++ src/main/java/edu/vnu/uet/smm/crawler/XMLWriter.java
@@ -0,0 +1,77 @@
+package edu.vnu.uet.smm.crawler;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.*;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+
+import edu.vnu.uet.smm.common.base.SMMDocument;
+
+public class XMLWriter {
+	private static void appendToElement(Document doc, Element parentElement, String name, String text) {
+		if (text == null) 
+			text = " ";
+		Element childElement = doc.createElement(name);
+		childElement.appendChild(doc.createTextNode(text));
+		parentElement.appendChild(childElement);
+	}
+	
+	public static void writeToFile(SMMDocument smmdoc, String path){
+		try {
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			
+			// Root element <info>
+			Document doc = docBuilder.newDocument();
+			Element infoElement = doc.createElement("info");
+			doc.appendChild(infoElement);
+			
+			// News element
+			Element newsElement = doc.createElement("news");
+			infoElement.appendChild(newsElement);
+			
+			// Link in news
+			appendToElement(doc, newsElement, "link", smmdoc.getLink());
+			// Category in news
+			appendToElement(doc, newsElement, "category", smmdoc.getCategory());
+			// Date in news
+			appendToElement(doc, newsElement, "date", smmdoc.getCreateDate());
+			// Update in news
+			//appendToElement(doc, newsElement, "update", smmdoc.getLastUpdate()); // Cant invoke getLastUpdate() ????
+			// Title in news
+			appendToElement(doc, newsElement, "title", smmdoc.getTitle());
+			// Likes in news
+			appendToElement(doc, newsElement, "like", Integer.toString(smmdoc.getLike()));
+			// Content in news
+			appendToElement(doc, newsElement, "content", smmdoc.getContent());
+			
+			// Write to file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(path));
+			transformer.transform(source, result);
+			
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		  } catch (TransformerException tfe) {
+			  tfe.printStackTrace();
+		  } 
+	}
+}
